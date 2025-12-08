28c012c8e58b2a8140dfbb66d96675844324911e
mohamed4711 <medmedia816@gmail.com>
2025-12-08 09:57:57 +0100

    interface

---
+++ interface /inlcude_/sdl_window.hpp
@@ -0,0 +1,53 @@
+/* ***********************************************************
+
+	CApp.h
+	
+	The application class definition.
+	
+	This file forms part of the qbRayTrace project as described
+	in the series of videos on the QuantitativeBytes YouTube
+	channel.
+	
+	This code corresponds specifically to Episode 1 of the series,
+	which may be found here:
+	https://youtu.be/JN5yUrJPThI
+	
+	The whole series may be found on the QuantitativeBytes 
+	YouTube channel at:
+	www.youtube.com/c/QuantitativeBytes
+	
+	GPLv3 LICENSE
+	Copyright (c) 2021 Michael Bennett
+	
+***********************************************************/
+
+#ifndef CAPP_H
+#define CAPP_H
+
+#include <SDL2/SDL.h>
+
+
+class CApp
+{
+	public:
+		CApp();
+		
+		int OnExecute();
+		bool OnInit();
+		void OnEvent(SDL_Event *event);
+		void OnLoop();
+		void OnRender();
+		void OnExit();
+		
+	private:
+		// An instance of qbImage to store the image.
+		qbImage m_image;
+	
+		// SDL2 stuff.
+		bool isRunning;
+		SDL_Window *pWindow;
+		SDL_Renderer *pRenderer;
+
+};
+
+#endif
